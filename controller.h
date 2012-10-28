ca2ea842c761fbed9b11ce015ae4411d3eb745a2
Sampo Laaksonen <sampo.laaksonen@gmail.com>
2012-10-28 17:26:46 +0200

    Remove axis normalization, change stick axis type

--- controller.h
+++ controller.h
@@ -6,21 +6,14 @@
 #define STICK_ZERO 128
 
 struct Controller {
-  unsigned char left_stick_x;
-  unsigned char left_stick_y;
-  unsigned char left_trigger;
+  signed char left_stick_x;
+  signed char left_stick_y;
 
-  unsigned char right_stick_x;
-  unsigned char right_stick_y;
+  signed char right_stick_x;
+  signed char right_stick_y;
+
   unsigned char right_trigger;
-
-  unsigned char left_stick_x_delta;
-  unsigned char left_stick_y_delta;
-  unsigned char left_trigger_delta;
-
-  unsigned char right_stick_x_delta;
-  unsigned char right_stick_y_delta;
-  unsigned char right_trigger_delta;
+  unsigned char left_trigger;
 
   int white;
   int black;
@@ -44,9 +37,6 @@ struct Controller {
   void updateDpad(unsigned char);
   void updateFromDataArray(unsigned char *);
   void print(HardwareSerial *);
-
-  unsigned char axisDelta(unsigned char *);
-  void updateDeltas(void);
 };
 
 #endif
