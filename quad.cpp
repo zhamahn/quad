621336cedc4d464e410285af461253efec18383d
Sampo Laaksonen <sampo.laaksonen@gmail.com>
2012-10-30 18:50:24 +0200

    Initialize altitudePID

--- quad.cpp
+++ quad.cpp
@@ -5,9 +5,11 @@
 Quad::Quad(void) {
   pitchPID = new PID(&pitchInput, &pitchOutput, &pitchSetpoint, KP, KI, KD, AUTOMATIC);
   rollPID = new PID(&rollInput, &rollOutput, &rollSetpoint, KP, KI, KD, AUTOMATIC);
+  altitudePID = new PID(&altitudeInput, &altitudeOutput, &altitudeSetpoint, KP, KI, KD, AUTOMATIC);
 
   pitchPID->SetOutputLimits(OUTPUT_MIN, OUTPUT_MAX);
   rollPID->SetOutputLimits(OUTPUT_MIN, OUTPUT_MAX);
+  altitudePID->SetOutputLimits(OUTPUT_MIN, OUTPUT_MAX);
 }
 
 void Quad::computePIDs(void) {
