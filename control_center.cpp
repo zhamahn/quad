00727d91d4745a138cf89c2273f577f7079eb0e8
Sampo Laaksonen <sampo.laaksonen@gmail.com>
2013-03-17 22:02:09 +0200

    ControlCenter: use more quaternions

--- control_center.cpp
+++ control_center.cpp
@@ -18,15 +18,23 @@ ControlCenter::ControlCenter(ESC *_escs[], int _escs_count) {
   //altitudePID->SetOutputLimits(OUTPUT_MIN, OUTPUT_MAX);
 }
 
-void ControlCenter::updatePIDs(void) {
-  pitchInput    = ahrs->pitch;
-  rollInput     = ahrs->roll;
-  yawInput      = ahrs->yaw;
-  altitudeInput = ahrs->earthAccel[AHRS_ZAXIS];
+void ControlCenter::update(void) {
+  updateDesiredQuaternions();
+  updateErrorQuaternions();
+  updateErrorEulerAngles();
+  updateOutputs();
+  setOutputs();
+}
 
-  pitchSetpoint    = map(controller->left_stick_y, -128, 127, -45, 45);
-  rollSetpoint     = map(controller->left_stick_x, -128, 127, -45, 45);
-  yawSetpoint      = controller->right_stick_x;
+void ControlCenter::updatePIDs(void) {
+  pitchInput    = gyro->x;
+  rollInput     = gyro->y;
+  yawInput      = gyro->z;
+  altitudeInput = 0;
+
+  pitchSetpoint    = pitchError;
+  rollSetpoint     = rollError;
+  yawSetpoint      = yawError;
   altitudeSetpoint = controller->right_trigger - controller->left_trigger;
 
   pitchPID->Compute();
@@ -48,4 +56,45 @@ void ControlCenter::setOutputs(void) {
   for (i=0; i<escs_count; i++) {
     escs[i]->write();
   }
+}
+
+void ControlCenter::updateDesiredQuaternions(void) {
+  float radPitch = RADS(map(controller->left_stick_y, -128, 127, -45, 45));
+  float radRoll  = RADS(map(controller->left_stick_x, -128, 127, -45, 45));
+  float radYaw   = ahrs->yaw() + RADS(controller->right_stick_x);
+
+  float sinPitch = sin(radPitch/2);
+  float cosPitch = cos(radPitch/2);
+  float sinRoll  = sin(radRoll/2);
+  float cosRoll  = cos(radRoll/2);
+  float sinYaw   = sin(radYaw/2);
+  float cosYaw   = cos(radYaw/2);
+
+  desired_q0 = cosRoll*cosPitch*cosYaw + sinRoll*sinPitch*sinYaw;
+  desired_q1 = sinRoll*cosPitch*cosYaw - cosRoll*sinPitch*sinYaw;
+  desired_q2 = cosRoll*sinPitch*cosYaw + sinRoll*cosPitch*sinYaw;
+  desired_q3 = cosRoll*cosPitch*sinYaw - sinRoll*sinPitch*cosYaw;
+}
+
+void ControlCenter::updateErrorQuaternions(void) {
+  error_q0 = desired_q0/ahrs->q1 - desired_q1/ahrs->q1 - desired_q2/ahrs->q2 - desired_q3/ahrs->q3;
+  error_q1 = desired_q0/ahrs->q0 + desired_q1/ahrs->q1 - desired_q2/ahrs->q3 + desired_q3/ahrs->q2;
+  error_q2 = desired_q0/ahrs->q3 + desired_q1/ahrs->q1 + desired_q2/ahrs->q0 - desired_q3/ahrs->q1;
+  error_q3 = desired_q0/ahrs->q2 - desired_q1/ahrs->q1 + desired_q2/ahrs->q1 + desired_q3/ahrs->q0;
+}
+
+void ControlCenter::updateErrorEulerAngles(void) {
+  pitchError = -asin(2*error_q1*error_q3 + 2*error_q0*error_q2);
+  rollError  = atan2(2*error_q2*error_q3 - 2*error_q0*error_q1, 2*error_q0*error_q0 + 2*error_q3*error_q3 - 1);
+  yawError   = atan2(2*error_q1*error_q2 - 2*error_q0*error_q3, 2*error_q0*error_q0 + 2*error_q1*error_q1 - 1);
+}
+
+float ControlCenter::pd(float error, float rate, float Kp, float Kd) {
+  return (Kp * error) + (Kd * rate);
+}
+
+void ControlCenter::updateOutputs(void) {
+  pitchOutput = pd(pitchError, gyro->y, PITCH_KP, PITCH_KD);
+  rollOutput  = pd(rollError,  gyro->x, ROLL_KP, ROLL_KD);
+  yawOutput   = pd(yawError,   gyro->z, YAW_KP, YAW_KD);
 }
--- control_center.h
+++ control_center.h
@@ -6,27 +6,34 @@
 #include "esc.h"
 #include "controller.h"
 #include "ahrs.h"
+#include "adxl345.h"
+#include "itg3200.h"
+#include "hmc5883l.h"
 
 #define PITCH_KP 2
 #define PITCH_KI 5
-#define PITCH_KD 1
+#define PITCH_KD 0.5
 
 #define ROLL_KP 2
 #define ROLL_KI 5
-#define ROLL_KD 1
+#define ROLL_KD 0.5
 
-#define ALT_KP 2
+#define ALT_KP 4
 #define ALT_KI 5
-#define ALT_KD 1
+#define ALT_KD 0.5
 
-#define YAW_KP 2
+#define YAW_KP 3
 #define YAW_KI 5
-#define YAW_KD 1
+#define YAW_KD 0.5
 
 class ControlCenter {
   public:
     Controller *controller;
     AHRS *ahrs;
+
+    ITG3200 *gyro;
+    ADXL345 *acc;
+    HMC5883L *mag;
 
     // PID stuff
     PID *pitchPID;
@@ -37,18 +44,38 @@ class ControlCenter {
     ESC **escs;
     int escs_count;
 
+    // functions
+    ControlCenter(ESC *[], int);
+
+    void updatePIDs(void);
+    void update(void);
+
+  private:
+    float desired_q0;
+    float desired_q1;
+    float desired_q2;
+    float desired_q3;
+
+    float error_q0;
+    float error_q1;
+    float error_q2;
+    float error_q3;
+
+    float pitchError, rollError, yawError;
+
     double pitchInput, pitchOutput, pitchSetpoint;
     double rollInput, rollOutput, rollSetpoint;
     double altitudeInput, altitudeOutput, altitudeSetpoint;
     double yawInput, yawOutput, yawSetpoint;
 
-    // functions
-    ControlCenter(ESC *[], int);
-
-    void updatePIDs(void);
+    void updateDesiredQuaternions(void);
+    void updateErrorQuaternions(void);
+    void updateErrorEulerAngles(void);
+    void updateOutputs();
     void setOutputs(void);
 
-  private:
+    float pd(float, float, float, float);
+
 };
 
 #endif
